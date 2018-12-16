34dee2110ec6037ef17a061360f07d79324ea989
Ghataorhe <b.ghataorhe@gmail.com>
2018-12-16 15:44:01 -0800

    completes schema

--- api/models/todoListModel.js
+++ api/models/todoListModel.js
@@ -1,3 +1,24 @@
 //database
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+//mongoDB Schema
+var taskSchema = new Schema({
+    name: {
+        type: String,
+        require: 'Please enter the name of the taks'
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: [{
+            type: String,
+            enum: ['pending', 'ongoing', 'completed']
+        }],
+        default: ['pending']
+    }
+});
+
+module.exports = mongoogse.model('Tasks', taskSchema);
